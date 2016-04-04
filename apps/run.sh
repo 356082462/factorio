276c9897ce8027be9835370a1b593551b3693183
zhentao.deng <zhentao.deng@imike.com>
2016-04-04 21:58:55 +0800

    factorio

--- apps/run.sh
+++ apps/run.sh
@@ -1,9 +1,9 @@
 if [ ${version} = "0.12.20" ]
 then
- 	/apps/factorio01220/bin/x64/factorio --latency-ms 30 --autosave-interval 30 --autosave-slots 6 --disallow-commands --start-server game-save
+ 	/apps/factorio01220/bin/x64/factorio --latency-ms 30 --autosave-interval 30 --autosave-slots 6 --disallow-commands --peer-to-peer --start-server game-save
 elif [ ${version} = "0.12.29" ]
 then
-	/apps/factorio01229/bin/x64/factorio --latency-ms 30 --autosave-interval 30 --autosave-slots 6 --disallow-commands --start-server game-save
+	/apps/factorio01229/bin/x64/factorio --latency-ms 30 --autosave-interval 30 --autosave-slots 6 --disallow-commands --peer-to-peer --start-server game-save
 else
 	echo "${version} not find!"
 fi
