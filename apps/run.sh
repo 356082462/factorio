ac863b4167101db134ca3706be790336e11ae041
zhentao.deng <zhentao.deng@imike.com>
2016-04-06 06:15:20 +0800

    factorio

--- apps/run.sh
+++ apps/run.sh
@@ -1,9 +1,12 @@
 if [ ${version} = "0.12.20" ]
 then
- 	/apps/factorio01220/bin/x64/factorio --latency-ms ${latencyMs} --autosave-interval ${autosaveInterval} --autosave-slots ${autosaveSlots} --disallow-commands --peer-to-peer --start-server game-save
+/apps/factorio01220/bin/x64/factorio --latency-ms ${latencyMs} --autosave-interval ${autosaveInterval} --autosave-slots ${autosaveSlots} --disallow-commands --peer-to-peer --start-server game-save
 elif [ ${version} = "0.12.29" ]
 then
-	/apps/factorio01229/bin/x64/factorio --latency-ms ${latencyMs} --autosave-interval ${autosaveInterval} --autosave-slots ${autosaveSlots} --disallow-commands --peer-to-peer --start-server game-save
+/apps/factorio01229/bin/x64/factorio --latency-ms ${latencyMs} --autosave-interval ${autosaveInterval} --autosave-slots ${autosaveSlots} --disallow-commands --peer-to-peer --start-server game-save
+elif [ ${version} = "0.12.30" ]
+then
+/apps/factorio01230/bin/x64/factorio --latency-ms ${latencyMs} --autosave-interval ${autosaveInterval} --autosave-slots ${autosaveSlots} --disallow-commands --peer-to-peer --start-server game-save
 else
-	echo "${version} not find!"
+echo "${version} not find!"
 fi
